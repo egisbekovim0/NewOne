ccbc4af68a29ff209b66390245d2e60f40f07cc3
yegisbekovyerlan <egisbekovim0@gmail.com>
2023-02-09 16:10:33 +0600

    jas

--- src/Student.java
+++ src/Student.java
@@ -2,7 +2,7 @@ import java.util.List;
 public class Student {
     private String name;
 
-
+    int b = 5;
     private List<Course> courses;
 
     public Student(String name, List<Course> courses) {
